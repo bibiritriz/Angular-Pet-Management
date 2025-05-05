973934687a0fecafdea04d7f119f5cda29863a51
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-05-05 18:41:06 -0300

    :sparkles: feat: create routes, edit component styles, and configure navbar and buttons links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PetsComponent } from './pets/pets.component';
+import { PetFormComponent } from './pet-form/pet-form.component';
+import { OwnersComponent } from './owners/owners.component';
+import { OwnerFormComponent } from './owner-form/owner-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'pets', component: PetsComponent
+  },
+  {
+    path: 'pet/new', component: PetFormComponent
+  },
+  {
+    path: 'pet/edit/:id', component: PetFormComponent
+  },
+  {
+    path: 'owners', component: OwnersComponent
+  },
+  {
+    path: 'owner/new', component: OwnerFormComponent
+  },
+  {
+    path: 'owner/edit/:id', component: OwnerFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
