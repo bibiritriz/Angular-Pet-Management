6eca6ce622ecf9717dcbcbef2f48b05a0cccdce6
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-05-05 19:43:43 -0300

    :sparkles: feat: created owner service

---
+++ src/app/owner.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Owner } from './pets/owner';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OwnerService {
+  apiUrl: string = 'http://localhost:3000/owner';
+
+  constructor(private readonly http: HttpClient) {}
+
+  getOwners(): Observable<Owner[]> {
+    return this.http.get<Owner[]>(this.apiUrl);
+  }
+
+  getOwner(): Observable<Owner> {
+    return this.http.get<Owner>(this.apiUrl);
+  }
+
+  updateOwner(owner: Owner): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${owner.id}`, owner);
+  }
+
+  deleteOwner(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  postOwner(owner: Owner): Observable<void> {
+    return this.http.post<void>(this.apiUrl, owner);
+  }
+}
